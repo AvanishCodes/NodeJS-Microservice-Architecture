ea2236fae81ef1fe8b40f365ac1ef47888616198
Avanish Gupta <avanishcodes@gmail.com>
2022-01-28 20:20:17 +0530

    Custom error objects

---
+++ user/errors/index.js
@@ -0,0 +1,21 @@
+InvalidCredentialsError = {
+    status: 'error',
+    message: 'Invalid credentials'
+}
+
+UserDoesNotExistError = {
+    status: 'error',
+    message: 'User does not exist'
+}
+
+UserAlreadyExistsError = {
+    status: 'error',
+    message: 'User already exists'
+}
+
+module.exports = {
+    InvalidCredentialsError,
+    UserDoesNotExistError,
+    
+}
+
