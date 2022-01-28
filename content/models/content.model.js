b477110ec946d0d3d6834c4466735f20c6017e49
Avanish Gupta <avanishcodes@gmail.com>
2022-01-28 15:25:06 +0530

    Add content model

---
+++ content/models/content.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const { ObjectID } = mongoose.Schema.Types;
+
+const contentSchema = new mongoose.Schema({
+    serial: {
+        type: Number,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    story: {
+        type: String,
+        required: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    publication_date: {
+        type: Date,
+        default: Date.now,
+    }
+})
+
+const content = mongoose.model("Content", contentSchema)
+
+module.exports = content
